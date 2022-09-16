66ec63db84255435f5d99477f3d58d65e1267b2f
Marina Ilyushina <m_ilyushina@hotmail.com>
2022-09-16 09:24:34 +0200

    todo list fixed

--- index.js
+++ index.js
@@ -2,46 +2,82 @@
 const input = document.querySelector("#input");
 const btn = document.querySelector("#btn");
 const list = document.querySelector("#list");
+const total = document.querySelector("#total");
 
-btn.addEventListener('click', addTodoHandler)
 
-todos = [];
+const todos = [
+    { done: false, text: '' }
 
-function addTodoHandler() {
-    let val = input.value;
-    console.log(val);
-    input.value = '';
+];
 
-    todos.push(val);
-    console.log(todos);
 
-    addTodo()
-}
-function addTodo() {
-    let value = '';
-    for (const todo of todos) {
-        value = todo;
+input.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        const newInput = {
+            done: false,
+            text: input.value
+        }
+        todos.unshift(newInput)
+        input.value = '';
+        render()
     }
-    list.innerHTML += `
-        <li>${value}</li>
-    `;
+})
 
+btn.addEventListener('click', function addTodoHandler() {
+    const newInput = {
+        done: false,
+        text: input.value
+    }
+    todos.unshift(newInput)
+    input.value = '';
+    render()
+})
+function render() {
+
+    const myListAsHTML = todos.map((item, i) =>
+        `<li >
+        ${item.done}
+        ${item.text}
+         <button onclick="check(${i})"><i class = 'fas fa-check'></i></button>
+        <button onclick="remove(${i})"><i class = 'fas fa-trash'></i></button>
+         </li>`)
+        .join("")
+
+    list.innerHTML = myListAsHTML
+    console.log(myListAsHTML);
+
+    /*  for (const todo of todos) {
+         value = todo;
+     }
+     list.innerHTML += `
+         <li>
+         ${value.text}
+         </li>
+             `; */
+}
+render()
+
+function toggleDone(i) {
+    todos[i].done = !todos[i]
+    console.log(todos[i])
+    render()
 }
 
+function remove(i) {
+    todos.splice(i, 1)
+    render()
+}
+
+render()
+
+let showOnlyPending = false;
+pendingToggleEl.addEventListener('click', function (event) {
+    showOnlyPending = !showOnlyPending
+    pendingToggleEl.innerText = showOnlyPending ? "remove filter" : "show only pending"
+    render()
+    console.log(showOnlyPending)
+
+})
 
 
-
-
-/* function addTodo(event) {
-
-      console.log(event.target.value)
-       console.log(todoInput.value) 
-   if (event.key === "Enter") {
-       myWeekTodo.push(todoInput.value)
-       console.log(myWeekTodo)
-       input.value = "";
-
-console.log("Done!");
-
- */
 
